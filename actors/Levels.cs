335c6a39a10d01dd1e41a58a025bced94b963aca
Quadtree <quadtree@gmail.com>
2023-01-07 00:39:09 -0800

    U

--- actors/Levels.cs
+++ actors/Levels.cs
@@ -12,6 +12,8 @@ public class Level1 : Level
     {
         base.CreateLevel(grid);
 
+        grid.AddTubWall(new IntVec2())
+
         for (var i = 0; i < 4; ++i)
         {
             grid.AddFluid(new IntVec2(10 + i, 14), FluidType.Green, 1_000_000);

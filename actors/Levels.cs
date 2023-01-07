72933e40b981c025650978371982766910207f98
Quadtree <quadtree@gmail.com>
2023-01-07 00:43:13 -0800

    U

--- actors/Levels.cs
+++ actors/Levels.cs
@@ -5,14 +5,14 @@ public class Level
 
     }
 
-    public void CreateFluidTub(GameGrid grid, IntVec2 bottomLeft, int width)
+    public void CreateFluidTub(GameGrid grid, IntVec2 bottomLeft, int width, FluidType type)
     {
         grid.AddTubWall(bottomLeft);
         grid.AddTubWall(bottomLeft + new IntVec2(0, 1));
 
         for (var i = 0; i < width; ++i)
         {
-            grid.AddFluid(bottomLeft + new IntVec2(i + 1, 1), FluidType.Green, 1_000_000);
+            grid.AddFluid(bottomLeft + new IntVec2(i + 1, 1), type, 1_000_000);
             grid.AddTubWall(bottomLeft + new IntVec2(i + 1, 0));
         }
 
@@ -27,6 +27,8 @@ public class Level1 : Level
     {
         base.CreateLevel(grid);
 
-        CreateFluidTub(grid, new IntVec2(10, 13), 3);
+        CreateFluidTub(grid, new IntVec2(10, 1), 3, FluidType.Green);
+
+        CreateFluidTub(grid, new IntVec2(6, 1), 3, FluidType.Blue);
     }
 }

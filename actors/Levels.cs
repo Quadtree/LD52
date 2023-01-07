f5a3b028b112b1a9d1115f1ac57000e39b40aab2
Quadtree <quadtree@gmail.com>
2023-01-07 00:32:19 -0800

    U

---
+++ actors/Levels.cs
@@ -0,0 +1,17 @@
+public class Level
+{
+    public virtual void CreateLevel(GameGrid grid)
+    {
+
+    }
+}
+
+public class Level1 : Level
+{
+    public override void CreateLevel(GameGrid grid)
+    {
+        base.CreateLevel(grid);
+
+        
+    }
+}

937d97e2e38754b8845193cfa7be77bcf360de38
Quadtree <quadtree@gmail.com>
2023-01-07 01:15:33 -0800

    U

--- actors/FluidNetwork.cs
+++ actors/FluidNetwork.cs
@@ -7,8 +7,17 @@ public class FluidNetwork
     public IntVec2[] Pumps;
     public IntVec2[] Outlets;
 
+    const int NUM_FLUID_TYPES = 3;
+
     public void Update()
     {
-        
+        for (var pump in Pumps)
+        {
+            for (int fluidType = 0; fluidType < NUM_FLUID_TYPES; ++fluidType)
+            {
+
+            }
+        }
+
     }
 }

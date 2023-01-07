ec42ee009ff3e688334b4d5dcf586496368752eb
Quadtree <quadtree@gmail.com>
2023-01-07 01:48:12 -0800

    U

--- actors/plants/Plant.cs
+++ actors/plants/Plant.cs
@@ -12,18 +12,28 @@ public class Plant : Spatial
     [Export]
     public int BlueUsedPerTick;
 
+    [Export]
+    public int MaxGrowth;
+
     public GameGrid Grid;
 
     public IntVec2 Pos;
+
+    public int Growth;
 
     public override void _Ready()
     {
 
     }
 
-    public override void _Process(float delta)
+    public override void _PhysicsProcess(float delta)
     {
-
+        if (Grid.Fluid[Pos.x, Pos.y, (int)FluidType.Red] >= RedUsedPerTick && Grid.Fluid[Pos.x, Pos.y, (int)FluidType.Green] >= GreenUsedPerTick && Grid.Fluid[Pos.x, Pos.y, (int)FluidType.Blue] >= BlueUsedPerTick)
+        {
+            Grid.Fluid[Pos.x, Pos.y, (int)FluidType.Red] -= RedUsedPerTick;
+            Grid.Fluid[Pos.x, Pos.y, (int)FluidType.Green] -= GreenUsedPerTick;
+            Grid.Fluid[Pos.x, Pos.y, (int)FluidType.Blue] -= BlueUsedPerTick;
+        }
     }
 
     public void Reposition(GameGrid gg, IntVec2 pos)

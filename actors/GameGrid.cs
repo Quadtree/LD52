06570995b841b77a552d653efa8f06d2f0d2fece
Quadtree <quadtree@gmail.com>
2023-01-06 17:53:12 -0800

    U

--- actors/GameGrid.cs
+++ actors/GameGrid.cs
@@ -3,6 +3,8 @@ using Godot;
 
 public class GameGrid : Spatial
 {
+    Placables? PlaceableSelected;
+
     public override void _Ready()
     {
 
@@ -11,5 +13,15 @@ public class GameGrid : Spatial
     public override void _Process(float delta)
     {
 
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        base._UnhandledInput(@event);
+
+        if (@event.IsActionPressed("select_item_0"))
+        {
+            PlaceableSelected = Placables.TubWall;
+        }
     }
 }

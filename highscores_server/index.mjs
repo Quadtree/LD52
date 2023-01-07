c5732ff5125447ab02cd5615c86a837ec863f353
Quadtree <quadtree@gmail.com>
2023-01-07 15:45:57 -0800

    U

--- highscores_server/index.mjs
+++ highscores_server/index.mjs
@@ -1,4 +1,4 @@
-import * as s3 from '@aws-sdk/client-s3';
+import { S3Client as s3 } from '@aws-sdk/client-s3';
 
 //const s3 = new aws.S3({apiVersion: "2006-03-01"});
 
@@ -7,7 +7,17 @@ const KEY_NAME = "scores.json";
 
 
 export const handler = async (event) => {
-    let object = {};
+
+    if (!event.queryStringParameters.data) throw new Error();
+
+    const incomingData = JSON.parse(event.queryStringParameters.data);
+
+    if (!incomingData.level) throw new Error();
+    if (!incomingData.timeSeconds) throw new Error();
+
+    let object = {
+        levelScores: {}
+    };
 
     try {
         object = await s3.getObject({
@@ -16,12 +26,22 @@ export const handler = async (event) => {
         });
     } catch (err) { }
 
+    if (typeof (object.levelScores[incomingData.level]) === "undefined") object.levelScores[incomingData.level] = [];
+
+    object.levelScores[incomingData.level].push(incomingData);
+
+    await s3.upload({
+        Bucket: BUCKET_NAME,
+        Key: KEY_NAME,
+        Body: JSON.stringify(object),
+    });
+
     const response = {
         statusCode: 200,
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ data: object }),
+        body: JSON.stringify({ scoresForThisLevel: object.levelScores[incomingData.level] }),
     };
     return response;
 };
--- highscores_server/notes.md
+++ highscores_server/notes.md
@@ -8,3 +8,5 @@ API endpoint is:
 
     https://k1seztx1s2.execute-api.us-west-2.amazonaws.com/default/ld52-scores
 
+    https://k1seztx1s2.execute-api.us-west-2.amazonaws.com/default/ld52-scores?data={%22level%22:%22level1%22,%22timeSeconds%22:55,%22completionState%22:%22goalReached%22,%22harvested%22:{%22FoodLeaf%22:1}}
+

7e75eae20ae1a7696d9921c4aec3762748d45698
Quadtree <quadtree@gmail.com>
2023-01-07 21:03:35 -0800

    U

--- actors/GameGrid.cs
+++ actors/GameGrid.cs
@@ -85,13 +85,25 @@ public class GameGrid : Spatial
                 {
                     backWall.Multimesh.SetInstanceTransform(x + y * (WIDTH + 2), new Transform(Quat.Identity, new Vector3(-1000, -1000, 0)));
                 }
-                else if (x == 0 || x == WIDTH + 1)
+                else if (x == 0)
                 {
-                    backWall.Multimesh.SetInstanceTransform(x + y * (WIDTH + 2), new Transform(new Quat(new Vector3(0, Mathf.Pi / 2, 0)), TileToVector(new IntVec2(x - 1, y - 1)) + new Vector3(0.5f, 0.5f, 0.0f)));
+                    backWall.Multimesh.SetInstanceTransform(x + y * (WIDTH + 2), new Transform(new Quat(new Vector3(0, Mathf.Pi / 2, 0)), TileToVector(new IntVec2(x - 1, y - 1)) + new Vector3(1.0f, 0.5f, .5f)));
+                }
+                else if (x == WIDTH + 1)
+                {
+                    backWall.Multimesh.SetInstanceTransform(x + y * (WIDTH + 2), new Transform(new Quat(new Vector3(0, Mathf.Pi / 2, 0)), TileToVector(new IntVec2(x - 1, y - 1)) + new Vector3(0.0f, 0.5f, .5f)));
+                }
+                else if (y == 0)
+                {
+                    backWall.Multimesh.SetInstanceTransform(x + y * (WIDTH + 2), new Transform(new Quat(new Vector3(0, Mathf.Pi / 2, 0)), TileToVector(new IntVec2(x - 1, y - 1)) + new Vector3(1.0f, 0.5f, .5f)));
+                }
+                else if (y == HEIGHT + 1)
+                {
+                    backWall.Multimesh.SetInstanceTransform(x + y * (WIDTH + 2), new Transform(new Quat(new Vector3(0, Mathf.Pi / 2, 0)), TileToVector(new IntVec2(x - 1, y - 1)) + new Vector3(0.0f, 0.5f, .5f)));
                 }
                 else
                 {
-                    backWall.Multimesh.SetInstanceTransform(x + y * (WIDTH + 2), new Transform(Quat.Identity, TileToVector(new IntVec2(x - 1, y - 1)) + new Vector3(0.5f, 0.5f, 0.0f)));
+                    backWall.Multimesh.SetInstanceTransform(x + y * (WIDTH + 2), new Transform(Quat.Identity, TileToVector(new IntVec2(x - 1, y - 1)) + new Vector3(0.0f, 0.0f, 0.0f)));
                 }
             }
         }

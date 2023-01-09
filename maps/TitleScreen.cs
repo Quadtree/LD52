fbabf6476626b0c975cd03484ffb2d1fe1b0ba8d
Quadtree <quadtree@gmail.com>
2023-01-09 00:53:55 -0800

    U

--- maps/TitleScreen.cs
+++ maps/TitleScreen.cs
@@ -6,11 +6,17 @@ public class TitleScreen : Control
     public override void _Ready()
     {
         this.FindChildByName<Button>("StartButton").Connect("pressed", this, nameof(StartButtonPressed));
+        this.FindChildByName<Button>("LevelSelectButton").Connect("pressed", this, nameof(LevelSelectButtonPressed));
     }
 
     void StartButtonPressed()
     {
         Level.CurrentLevel = 0;
         GetTree().ChangeScene("res://maps/Default.tscn");
+    }
+
+    void LevelSelectButtonPressed()
+    {
+        this.FindChildByName<PopupDialog>("LevelSelectDialog").PopupCentered();
     }
 }
